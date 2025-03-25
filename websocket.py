650fd30e3e9cc6dd5e62c804d034067c4c8407a0
Angel <angel@192.168.0.101>
2025-03-25 09:06:18 -0500

    Endpoint of btc and bnb

--- websocket.py
+++ websocket.py
@@ -54,6 +54,8 @@ from datetime import datetime
 import threading
 from flask import Flask, jsonify, send_file
 import io
+import matplotlib.ticker as mticker
+
 
 # Listas para almacenar los datos
 prices_btc, times_btc = [], []
@@ -68,8 +70,8 @@ def hola():
 @app.route('/last_values', methods=['GET'])
 def last_values():
     return jsonify({
-        "BTC": list(zip(times_btc[-10:], prices_btc[-10:])) if len(prices_btc) >= 10 else list(zip(times_btc, prices_btc)),
-        "BNB": list(zip(times_bnb[-10:], prices_bnb[-10:])) if len(prices_bnb) >= 10 else list(zip(times_bnb, prices_bnb))
+        "BTC": list(zip(times_btc[-100:], prices_btc[-100:])) if len(prices_btc) >= 100 else list(zip(times_btc, prices_btc)),
+        "BNB": list(zip(times_bnb[-100:], prices_bnb[-100:])) if len(prices_bnb) >= 100 else list(zip(times_bnb, prices_bnb))
     })
 
 @app.route('/create_image', methods=['GET'])
@@ -99,6 +101,64 @@ def create_image():
 
     return send_file(img_io, mimetype='image/png')
 
+@app.route('/create_image/btc', methods=['GET'])
+def image_btc():
+    data = last_values().json
+    if not data["BTC"]:
+        return "No data in BTC"
+    print(data)
+    times_btc, prices_btc = zip(*data["BTC"])
+    print("PRICES")
+    print(prices_btc)
+    print("TIMES")
+    print(times_btc)
+
+    plt.figure(figsize=(8, 5))
+    plt.plot(times_btc, prices_btc, marker='o', linestyle='-', color='blue', label="BTC")
+    plt.xlabel("Tiempo")
+    plt.ylabel("Precio")
+    plt.title("Últimos 10 valores de BTC")
+    plt.legend()
+    plt.xticks(rotation=45)
+
+    plt.gca().yaxis.set_major_formatter(mticker.StrMethodFormatter("{x:,.2f}"))
+
+    img_io = io.BytesIO()
+    plt.savefig(img_io, format='png')
+    plt.close()
+    img_io.seek(0)
+
+    return send_file(img_io, mimetype='image/png')
+
+@app.route('/create_image/bnb', methods=['GET'])
+def image_bnb():
+    data = last_values().json
+    if not data["BTC"]:
+        return "No data in BNB"
+    print(data)
+    times_btc, prices_btc = zip(*data["BNB"])
+    print("PRICES")
+    print(prices_btc)
+    print("TIMES")
+    print(times_btc)
+
+    plt.figure(figsize=(8, 5))
+    plt.plot(times_bnb, prices_btc, marker='o', linestyle='-', color='blue', label="BNB")
+    plt.xlabel("Tiempo")
+    plt.ylabel("Precio")
+    plt.title("Últimos 10 valores de BNB")
+    plt.legend()
+    plt.xticks(rotation=45)
+
+    plt.gca().yaxis.set_major_formatter(mticker.StrMethodFormatter("{x:,.2f}"))
+
+    img_io = io.BytesIO()
+    plt.savefig(img_io, format='png')
+    plt.close()
+    img_io.seek(0)
+
+    return send_file(img_io, mimetype='image/png')
+
 async def listen_trades(symbol, prices, times):
     websocket_url = f"wss://stream.binance.com:9443/ws/{symbol}@trade"
     
@@ -109,9 +169,9 @@ async def listen_trades(symbol, prices, times):
                 message = await ws.recv()
                 data = json.loads(message)
                 trade_price = float(data['p'])
-                trade_time = datetime.now().strftime('%H:%M:%S')
+                trade_time = float(data['E'])
 
-                if len(prices) >= 10:
+                if len(prices) >= 100:
                     prices.pop(0)
                     times.pop(0)
 
@@ -129,4 +189,4 @@ def start_websocket(symbol, prices, times):
 if __name__ == "__main__":
     threading.Thread(target=start_websocket, args=("btcusdt", prices_btc, times_btc), daemon=True).start()
     threading.Thread(target=start_websocket, args=("bnbusdt", prices_bnb, times_bnb), daemon=True).start()
-    app.run(debug=True, use_reloader=False)
+    app.run(debug=True, use_reloader=True)
